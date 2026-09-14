50e98dc190a4ba8ac317b214735532f8cf826c39
toiladungx <dungluji@gmail.com>
2026-09-14 20:56:52 +0700

    First

---
+++ BaiTap3_PTB2/main.c
@@ -0,0 +1,17 @@
+//Viết chương trình giải phương trình bậc 2 trong C. Phương trình bậc 2 có dạng:
+//ax^2 + bx + c = 0
+
+#include <stdio.h>
+
+void main()
+{
+    float a, b, c;
+
+    printf("Nhap he so bac 2: a = \n");
+    scanf("%f", &a);
+    printf("Nhap he so bac 1: b = \n");
+    scanf("%f", &b);
+    printf("Nhap he so tu do: a = \n");
+    scanf("%f", &c);
+
+}
