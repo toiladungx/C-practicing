02c402f29f3112156ab72f1d29146451d7b462a7
toiladungx <dungluji@gmail.com>
2026-09-15 14:04:35 +0700

    Update code

--- 003_TimSoNguyenToNhoHonN/main.c
+++ 003_TimSoNguyenToNhoHonN/main.c
@@ -1,28 +1,47 @@
-// Viết chương trình liệt kê tất cả các số nguyên tố nhỏ hơn n.
+/*Đề bài: Viết chương trình liệt kê tất cả các số nguyên tố nhỏ hơn n.
 
-// Định nghĩa: Số nguyên tố là số lớn hơn 1 và chỉ chia hết cho 1 và chính nó.
-// Ví dụ: 2, 3, 5, 7, 11, 13, 17, … là những số nguyên tố.
-// Chú ý: Số 0 và 1 không phải là số nguyên tố. Chỉ có số 2 là số nguyên tố chẵn, 
-//tất cả các số chẵn khác không phải là số nguyên tố vì chúng chia hết cho 2.
+Định nghĩa: Số nguyên tố là số lớn hơn 1 và chỉ chia hết cho 1 và chính nó.
+Ví dụ: 2, 3, 5, 7, 11, 13, 17, … là những số nguyên tố.
+Chú ý: Số 0 và 1 không phải là số nguyên tố. Chỉ có số 2 là số nguyên tố chẵn, 
+tất cả các số chẵn khác không phải là số nguyên tố vì chúng chia hết cho 2.
 
-//2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+Kết quả:
+Nhập n = 100
+Tat ca cac so nguyen to nho hon 100 la: 
+2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+*/
 
 #include <stdio.h>
 
-void main()
+int main()
 {
-    int n, i;
+    int n;
+    int i, j;
+    int dem;
 
-    printf("Nhap so N:");
-    scanf("%d", &n);
+    do{
+        printf("Nhap so n (>0): ");
+        scanf("%d", &n);
+    }while(n <= 0);
+ 
+    printf("Tat ca cac so nguyen to nho hon %d la:\n", n);
 
-    printf("Tat ca so nguyen to nho hon %d la: ", n);
-
-    for(i = 0; i <= n; i++)
+    for(i = 2; i < n; i++)
     {
-        if( (i > 1) && ((i % 1) == 0) && ((i % i) == 0) )
+        dem = 0;
+        for(j = 1; j <= i; j++)
         {
-            printf("%d, ", i);
+            if(i % j == 0)
+            {
+                dem++;
+            }
+        }
+
+        if(dem == 2)
+        {
+            printf("%d ",i);
         }
     }
+
+    return 0;
 }
