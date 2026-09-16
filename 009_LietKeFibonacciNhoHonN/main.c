852e03b8c6234b1f54863a3b58924c5f887714df
toiladungx <dungluidloop@gmail.com>
2026-09-16 22:58:23 +0700

    Add 009

---
+++ 009_LietKeFibonacciNhoHonN/main.c
@@ -0,0 +1,8 @@
+/*
+Đề bài: Viết chương trình C liệt kê các số Fibonacci nhỏ hơn n là số nguyên tố.
+Số tự nhiên n được nhập từ bàn phím.
+
+Kết quả:
+Nhap so nguyen duong = 100
+Cac so fibonacci nho hon 100 va la so nguyen to: 2 3 5 13 89
+*/
